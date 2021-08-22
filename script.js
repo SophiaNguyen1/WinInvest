eee7b5cf9b86d2374fa91756362d555e58b789b2
MstrPikachu <31784486+MstrPikachu@users.noreply.github.com>
2021-08-22 00:52:25 -0400

    Fixed updating functions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-var total = 0; 
 /* event listener */
 document.getElementsByName("job")[0].addEventListener('change', calculate);
 document.getElementsByName("loans")[0].addEventListener('change', calculate);
@@ -16,14 +15,45 @@ document.getElementsByName("medical")[0].addEventListener('change', calculate2);
 document.getElementsByName("toiletries")[0].addEventListener('change', calculate2);
 document.getElementsByName("clothes")[0].addEventListener('change', calculate2);
 document.getElementsByName("debt")[0].addEventListener('change', calculate2);
-document.getElementsByName("loanpayments")[0].addEventListener('change', calculate2);
+document.getElementsByName("loanpayment")[0].addEventListener('change', calculate2);
 document.getElementsByName("other2")[0].addEventListener('change', calculate2);
 /* function */
 function calculate()
 {
-    total += this.value;
+	income = 0;
+	plus = ["job", "loans", "other"];
+	for (key in plus){
+		console.log(plus[key]);
+		input = document.getElementsByName(plus[key])[0].value;
+		matches = input.match(/^\d+(\.\d{1,2})?/);
+		if (matches && matches[0]){
+			income += parseFloat(matches[0]);
+		}
+	}
+	incomeElement = document.getElementById("income");
+	prevIncome = parseFloat(incomeElement.innerHTML.substring(1));
+	incomeElement.innerHTML = "$" + income.toFixed(2);
+	savingsElement = document.getElementById("savings");
+	prevSavings = parseFloat(savingsElement.innerHTML.substring(1));
+	savingsElement.innerHTML = "$" + (prevSavings - prevIncome + income).toFixed(2);
 }
 function calculate2()
 {
-    total -= this.value(); 
+	expenses = 0;
+	minus = ["housing", "utilities", "electronics", "food", "giving", "childcare", "petcare", "car", "medical", "toiletries", "clothes", "debt", "loanpayment", "other2"];
+	for (key in minus){
+		console.log(minus[key]);
+		input = document.getElementsByName(minus[key])[0].value;
+		matches = input.match(/^\d+(\.\d{1,2})?/);
+		if (matches && matches[0]){
+			expenses += parseFloat(matches[0]);
+		}
+	}
+	console.log(expenses);
+	expensesElement = document.getElementById("expenses");
+	prevExpenses = parseFloat(expensesElement.innerHTML.substring(1));
+	expensesElement.innerHTML = "$" + expenses.toFixed(2);
+	savingsElement = document.getElementById("savings");
+	prevSavings = parseFloat(savingsElement.innerHTML.substring(1));
+	savingsElement.innerHTML = "$" + (prevSavings + prevExpenses - expenses).toFixed(2);
 }
