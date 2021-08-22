bbe3ca975766c71153408234f8759196f6ab5eb9
MstrPikachu <31784486+MstrPikachu@users.noreply.github.com>
2021-08-22 01:36:56 -0400

    Finish calculator

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 
 /* event listener */
-document.getElementsByName("job")[0].addEventListener('change', calculate);
-document.getElementsByName("loans")[0].addEventListener('change', calculate);
-document.getElementsByName("other")[0].addEventListener('change', calculate);
-document.getElementsByName("housing")[0].addEventListener('change', calculate2);
-document.getElementsByName("utilities")[0].addEventListener('change', calculate2);
-document.getElementsByName("electronics")[0].addEventListener('change', calculate2);
-document.getElementsByName("food")[0].addEventListener('change', calculate2);
-document.getElementsByName("giving")[0].addEventListener('change', calculate2);
-document.getElementsByName("childcare")[0].addEventListener('change', calculate2);
-document.getElementsByName("petcare")[0].addEventListener('change', calculate2);
-document.getElementsByName("car")[0].addEventListener('change', calculate2);
-document.getElementsByName("medical")[0].addEventListener('change', calculate2);
-document.getElementsByName("toiletries")[0].addEventListener('change', calculate2);
-document.getElementsByName("clothes")[0].addEventListener('change', calculate2);
-document.getElementsByName("debt")[0].addEventListener('change', calculate2);
-document.getElementsByName("loanpayment")[0].addEventListener('change', calculate2);
-document.getElementsByName("other2")[0].addEventListener('change', calculate2);
+window.onload = function() {
+	//console.log("test1");
+	document.getElementById("job").addEventListener('change', calculate, false);
+	document.getElementById("loans").addEventListener('change', calculate, false);
+	document.getElementById("other").addEventListener('change', calculate, false);
+	document.getElementById("housing").addEventListener('change', calculate2, false);
+	document.getElementById("utilities").addEventListener('change', calculate2, false);
+	document.getElementById("electronics").addEventListener('change', calculate2, false);
+	document.getElementById("food").addEventListener('change', calculate2, false);
+	document.getElementById("giving").addEventListener('change', calculate2, false);
+	document.getElementById("childcare").addEventListener('change', calculate2, false);
+	document.getElementById("petcare").addEventListener('change', calculate2, false);
+	document.getElementById("car").addEventListener('change', calculate2, false);
+	document.getElementById("medical").addEventListener('change', calculate2, false);
+	document.getElementById("toiletries").addEventListener('change', calculate2, false);
+	document.getElementById("clothes").addEventListener('change', calculate2, false);
+	document.getElementById("debt").addEventListener('change', calculate2, false);
+	document.getElementById("loanpayment").addEventListener('change', calculate2, false);
+	document.getElementById("other2").addEventListener('change', calculate2, false);
+	//console.log("test2");
+}
 /* function */
 function calculate()
 {
 	income = 0;
 	plus = ["job", "loans", "other"];
 	for (key in plus){
-		console.log(plus[key]);
+		//console.log(plus[key]);
 		input = document.getElementsByName(plus[key])[0].value;
 		matches = input.match(/^\d+(\.\d{1,2})?/);
 		if (matches && matches[0]){
@@ -42,14 +46,14 @@ function calculate2()
 	expenses = 0;
 	minus = ["housing", "utilities", "electronics", "food", "giving", "childcare", "petcare", "car", "medical", "toiletries", "clothes", "debt", "loanpayment", "other2"];
 	for (key in minus){
-		console.log(minus[key]);
+		//console.log(minus[key]);
 		input = document.getElementsByName(minus[key])[0].value;
 		matches = input.match(/^\d+(\.\d{1,2})?/);
 		if (matches && matches[0]){
 			expenses += parseFloat(matches[0]);
 		}
 	}
-	console.log(expenses);
+	//console.log(expenses);
 	expensesElement = document.getElementById("expenses");
 	prevExpenses = parseFloat(expensesElement.innerHTML.substring(1));
 	expensesElement.innerHTML = "$" + expenses.toFixed(2);
